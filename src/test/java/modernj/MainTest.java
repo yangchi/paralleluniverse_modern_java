b2cab2978c22d1a48e605ac72543f98fc672141d
Yang CHI <yangchi@cisco.com>
2014-08-08 23:43:31 -0700

    Added the sample main code. Modified build.gradle

---
+++ src/test/java/modernj/MainTest.java
@@ -0,0 +1,12 @@
+package modernj;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MainTest {
+	@Test
+	public void testTriple() {
+		assertThat(Main.triple("AB"), equalTo("ABABAB"));
+	}
+}
