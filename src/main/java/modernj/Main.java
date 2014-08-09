2b858607673c3977d989ed2043cde2ca96370857
Yang CHI <yangchi@cisco.com>
2014-08-09 15:24:37 -0700

    Select-channel model

--- src/main/java/modernj/Main.java
+++ src/main/java/modernj/Main.java
@@ -1,9 +1,11 @@
 package modernj;
 
-import co.paralleluniverse.strands.channels.Channel;
-import co.paralleluniverse.strands.channels.Channels;
 import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.strands.Strand;
+import co.paralleluniverse.strands.channels.Channel;
+import co.paralleluniverse.strands.channels.Channels;
+import co.paralleluniverse.strands.channels.SelectAction;
+import static co.paralleluniverse.strands.channels.Selector.*;
 
 public class Main {
 
@@ -12,20 +14,37 @@ public class Main {
      * @param args
      */
     public static void main (String[] args) throws Exception{
-        final Channel<Integer> ch = Channels.newChannel(0);
+        final Channel<Integer> ch1 = Channels.newChannel(0);
+        final Channel<String> ch2 = Channels.newChannel(0);
         
         new Fiber<Void>(() -> {
             for (int i = 0; i < 10; i++){
                 Strand.sleep(100);
-                ch.send(i);
+                ch1.send(i);
             }
-            ch.close();
+            ch1.close();
         }).start();
         
         new Fiber<Void>(() -> {
-            Integer x;
-            while((x = ch.receive()) != null)
-                System.out.println("-->" + x);
+            for (int i = 0; i < 10; i++) {
+                Strand.sleep(130);
+                ch2.send(Character.toString((char)('a' + i)));
+            }
+            ch2.close();
+        }).start();
+        
+        new Fiber<Void>(() -> {
+            for (int i = 0; i < 10; i++) {
+                SelectAction<Object> sa = select(receive(ch1), receive(ch2));
+                switch (sa.index()) {
+                    case 0:
+                        System.out.println(sa.message() != null ? "Got a number: " + (int)sa.message() : "ch1 closed");
+                        break;
+                    case 1:
+                        System.out.println(sa.message() != null ? "Got a string: " + (String)sa.message() : "ch2 closed");
+                        break;
+                }
+            }
         }).start().join();
     }
     
