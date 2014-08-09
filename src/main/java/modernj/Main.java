7b64426c6cd7e9eccf6f0ff5281b93026a4081f3
Yang CHI <yangchi@cisco.com>
2014-08-09 15:00:11 -0700

    Concurrency via channel

--- src/main/java/modernj/Main.java
+++ src/main/java/modernj/Main.java
@@ -1,42 +1,32 @@
 package modernj;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.stream.Collectors;
-import static java.util.stream.Collectors.*;
-import static java.util.stream.IntStream.range;
+import co.paralleluniverse.strands.channels.Channel;
+import co.paralleluniverse.strands.channels.Channels;
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.strands.Strand;
 
 public class Main {
-    public static void main(String[] args) {
-        range(0, 100)
-                .mapToObj(i -> randomString(new Random(), 'A', 'Z', 10))
-                .sorted()
-                .collect(groupingBy(name -> name.charAt(0)))
-                .forEach((letter, name) -> System.out.println(letter + "\n\t" + name.stream().collect(joining("\n\t"))));
+
+    /**
+     *
+     * @param args
+     */
+    public static void main (String[] args) throws Exception{
+        final Channel<Integer> ch = Channels.newChannel(0);
+        
+        new Fiber<Void>(() -> {
+            for (int i = 0; i < 10; i++){
+                Strand.sleep(100);
+                ch.send(i);
+            }
+            ch.close();
+        }).start();
+        
+        new Fiber<Void>(() -> {
+            Integer x;
+            while((x = ch.receive()) != null)
+                System.out.println("-->" + x);
+        }).start().join();
     }
     
-    /**
-     * ## The Random String Generator
-     *
-     * This method doesn't do much, except for generating a random string. It:
-     *
-     *  * Generates a random string at a given length, `length`
-     *  * Uses only characters in the range given by `from` and `to`.
-     *
-     * Example:
-     *
-     * ```java
-     * randomString(new Random(), 'a', 'z', 10);
-     * ```
-     *
-     * @param r      the random number generator
-     * @param from   the first character in the character range, inclusive
-     * @param to     the last character in the character range, inclusive
-     * @param length the length of the generated string
-     * @return the generated string of length `length`
-     */
-    public static String randomString(Random r, char from, char to, int length){
-        return r.ints(from, to + 1).limit(length).mapToObj(x -> Character.toString((char)x)).collect(Collectors.joining());
-    }
 }
